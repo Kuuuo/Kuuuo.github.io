2a545821d3f030662cb599a0da1282af99e67ab7
KUUUO <36560912@qq.com>
2019-12-13 22:17:20 +0800

    Site updated: 2019-12-13 22:17:16

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/Kuuuo/BlogPhotos/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/Kuuuo/BlogPhotos/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/Kuuuo/Kuuuo.github.io/master/photos/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/Kuuuo/Kuuuo.github.io/master/photos/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
